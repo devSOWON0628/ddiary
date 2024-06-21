c0a7b34798d25742a7238b41e646d701573ee17e
SOWON <48439367+devSOWON0628@users.noreply.github.com>
2024-06-21 17:06:03 +0900

    디테일 화면

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,3 +1,53 @@
 <template>
-    디테일화면
+  <v-container>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card variant="outlined"> 
+          <v-card-title class="text-h5"> 6월 6일 </v-card-title>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </v-card-text>
+
+          <v-avatar class="ma-3" rounded="0" size="300">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+            ></v-img>
+          </v-avatar>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </v-card-text>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </v-card-text>
+
+          <v-avatar class="ma-3" rounded="0" size="300">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
+            ></v-img>
+          </v-avatar>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
